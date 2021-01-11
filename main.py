c3a680d812ab8966279b8146659da82e26f06158
DimaSvyrydenko <20019dima@gmail.com>
2021-01-11 12:49:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Komentarus var
+#rakstit vairakas
+#rindas
+print("Hello World")
+print(2 * 2)
+print(2 * 2, 2 * 3, 2 * 4, "pitons")
+print(f"Ja saskaitisim 5 ar 7, iegusim {5+7}.")  #Kombineta druka
+print("Sveika, " + "pasaulei!")
+
+print("Dmytro " * 5)
+print(365 * 24 * 60 * 60)
+
+print(14%4)
+print(2**3)
+print(14//4)
+print(2+10*10+3)
+print((2+10)*(10+3))
+print(0.1+0.2-0.3)
+print(1/2)
+print()
